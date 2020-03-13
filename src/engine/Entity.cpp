a3df2f0ea1fc27ad4392158de45420f7c3a18432
Andrew <oliver.lee@hotmail.co.uk>
2020-03-13 21:21:38 +0000

    added path movement and basic progress return

--- src/engine/Entity.cpp
+++ src/engine/Entity.cpp
@@ -1,7 +1,7 @@
 #include "Entity.h"
 
-Entity::Entity(int xPos, int yPos, int height, int width, bool bounding, SDL_Texture * image) : x(xPos), y(yPos), h(height), w(width), bBox(bounding), collider(0, 0, 0, 0) {
-	texture = image; //can we set bounding box based on image size?
+Entity::Entity(int xPos, int yPos, int height, int width, bool bounding, SDL_Texture * inputTexture) : x(xPos), y(yPos), h(height), w(width), bBox(bounding), collider(0, 0, 0, 0) {
+	texture = inputTexture; //can we set bounding box based on image size?
 	display = new SDL_Rect{ x, y, h, w }; 
 
 	if (bBox) {
@@ -34,6 +34,48 @@ void Entity::setSight(int dist) {
 
 int Entity::getSight() {
 	return sight;
+}
+
+void Entity::moveAlongPath() {
+	if (nodesPassed < pathCheck.size()) {
+		int destinationX = pathCheck[nodesPassed].x;
+		int destinationY = pathCheck[nodesPassed].y;
+		if ((w+1)*(destinationX) > x) {
+			moveX(speed);
+		}
+		else if ((w+1)*(destinationX) < x) {
+			moveX(-speed);
+		}
+		if ((w+1)*(destinationY) > y) {
+			moveY(speed);
+		}
+		else if ((w+1)*(destinationY) < y) {
+			moveY(-speed);
+		}
+		if ((w+1)*(destinationX) == x && (w+1)*(destinationY) == y) {
+			nodesPassed += 1;
+			pathCheck.erase(pathCheck.begin());
+		}
+	}
+}
+
+void Entity::aiMovePath(std::vector<Point2> path) {
+	pathCheck = path;
+}
+
+float Entity::getPathProgress() {
+	if (pathCheck.size() == 0) {
+		return 1;
+	}
+	if (nodesPassed == 0) {
+		return 0;
+	}
+	if (nodesPassed == pathCheck.size()) {
+		nodesPassed = 0;
+		return 1;
+	}
+	nodesPassed = 0;
+	return 0;
 }
 
 
--- src/engine/Entity.h
+++ src/engine/Entity.h
@@ -2,6 +2,7 @@
 #define __ENTITY_H__
 
 #include "GraphicsEngine.h"
+#include <vector>
 
 class Entity {
 	private:
@@ -9,18 +10,26 @@ class Entity {
 		void addBounding();
 		int h;
 		int w;
-		int sight = 0;
+		int sight = 1;
+		int nodesPassed = 0;
+		int speed = 1;
+		std::vector<Point2> pathCheck;
 	public:
 		int x;
 		int y;
+
 		Rect collider;
 		SDL_Texture * texture;
 		SDL_Rect * display;
+
 		Entity(int, int, int, int, bool, SDL_Texture *);
 		void moveX(int);
 		void moveY(int);
 		void setSight(int);
 		int getSight();
+		void moveAlongPath();
+		void aiMovePath(std::vector<Point2>);
+		float getPathProgress();
 };
 
 #endif
