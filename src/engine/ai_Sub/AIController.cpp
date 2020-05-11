eeac0671e00ddc886a8d4fe941dbbcb90105fdc8
Andrew Lee <oliver.lee@hotmail.co.uk>
2020-05-11 22:07:51 +0100

    ammended comments

--- src/engine/ai_Sub/AIController.cpp
+++ src/engine/ai_Sub/AIController.cpp
@@ -9,7 +9,7 @@ AIController::~AIController() {
 }
 
 void AIController::addMap(int tile, int xPixel, int yPixel, std::vector<std::shared_ptr<Rect>> map) {
-	//if tile cannot fit neatly into map size
+	// If tile cannot fit neatly into map size
 	if ((xPixel%tile != 0) || (yPixel%tile != 0)) {
 		try {
 			throw 1;
@@ -23,15 +23,15 @@ void AIController::addMap(int tile, int xPixel, int yPixel, std::vector<std::sha
 	mapX = xPixel / tile;
 	mapY = yPixel / tile;
 
-	//On occasion of n+1 maps/levels, Removes old Node data
+	// On occasion of n+1 maps/levels, Removes old Node data
 	walkable.clear();
 
-	//Following code creates a gameworld based on pixel size and tile size fed to it. 2d Vector data structure used for representation
+	// Following code creates a gameworld based on pixel size and tile size fed to it. 2d Vector data structure used for representation
 	walkable.resize(mapY);
 	bool wCheck = true;
 	for (int i = 0; i < mapX; i++) {
 		for (int j = 0; j < mapY; j++) {
-			Node push = Node(j, i, tileSize, tileSize);
+			Node push = Node(j, i, tileSize, tileSize); /** A Node generated based on map coordinates to be pushed onto walkable */
 
 			wCheck = true;
 			for (auto block : map) {
@@ -45,7 +45,7 @@ void AIController::addMap(int tile, int xPixel, int yPixel, std::vector<std::sha
 		}
 	}
 
-	//check and set moveable edges
+	// Check and set moveable edges
 	for (int i = 0; i < mapX; i++) {
 		for (int j = 0; j < mapY; j++) {
 			if (i > 0) {
@@ -70,25 +70,18 @@ void AIController::addMap(int tile, int xPixel, int yPixel, std::vector<std::sha
 			}
 		}
 	}
-
-	//The below code to print a visualisation of the game world to console is edited from "Vlad from Moscow" - https://stackoverflow.com/questions/42249303/how-can-i-push-back-data-in-a-2d-vector-of-type-int 05/03/2020
-	/*for (const auto v : walkable)
-	{
-		for (bool x : v) std::cout << x << ' ';
-		std::cout << std::endl;
-	}*/
 }
 
 void AIController::givePath(Entity* seeker, Entity* dest) {
-	int sight = seeker->getSight();
-	int seekerX = seeker->getX() / tileSize;
-	int seekerY = seeker->getY() / tileSize;
-	int destX = dest->getX() / tileSize;
-	int destY = dest->getY() / tileSize;
+	int sight = seeker->getSight(); /** The sight radius of seeker Entity */
+	int seekerX = seeker->getX() / tileSize; /** Tile location X of seeker Entity */
+	int seekerY = seeker->getY() / tileSize; /** Tile location Y of seeker Entity */
+	int destX = dest->getX() / tileSize;	/** Tile location X of destination Entity */
+	int destY = dest->getY() / tileSize;	/** Tile location Y of destination Entity */
 
 	path.clear();
-	//basic 'line of sight' currently does not take walls into account
-	bool sighted = false, complete = false;
+	// If second entity is in range
+	bool sighted = false;	/** Flag to show if Entity is in range */
 	if (sight > 0) {
 		if (Rect((seekerX * tileSize) - (sight * tileSize), (seekerY * tileSize) - (sight * tileSize),
 			(sight * 2) * tileSize, (sight * 2) * tileSize).intersects(dest->getCollider())) {
@@ -99,14 +92,14 @@ void AIController::givePath(Entity* seeker, Entity* dest) {
 		sighted = true; 
 	}
 
-	//if destination entity is in sight then chase otherwise, 'patrol'
+	// If destination entity is in sight then chase otherwise, check for patrol behaviour and react
 	if (sighted) {
 		sighted = false;
 		path = search.AStarSearch(Point2{ seekerX, seekerY }, Point2{ destX, destY }, walkable, 0);
 	}
 	else if (seeker->getPatrol()) {
-		int randomX = std::rand() % (sight + 1) - (sight / 2);
-		int randomY = std::rand() % (sight + 1) - (sight / 2);
+		int randomX = std::rand() % (sight + 1) - (sight / 2);	/** Random point X within sight range of Entity */
+		int randomY = std::rand() % (sight + 1) - (sight / 2);	/** Random point Y within sight range of Entity */
 		if (randomX + seekerX < 0) {
 			randomX *= -1;
 		}
@@ -136,39 +129,27 @@ void AIController::givePath(Entity* seeker, Point2 dest) {
 	int destX = dest.x / tileSize;
 	int destY = dest.y / tileSize;
 
-	if (seeker->getPatrol()) {
-		int randomX = std::rand() % (sight + 1) - (sight / 2);
-		int randomY = std::rand() % (sight + 1) - (sight / 2);
-
-		if (randomX + destX < 0) {
-			randomX *= -1;
-		}
-		if (randomY + destY < 0) {
-			randomY *= -1;
-		}
-		if (randomX + destX > mapX - 1) {
-			randomX *= -1;
-		}
-		if (randomY + destY > mapY - 1) {
-			randomY *= -1;
-		}
-
-		randomX += dest.x;
-		randomY += dest.y;
-
-		path = search.AStarSearch(Point2{ seekerX, seekerY }, Point2{ randomX, randomY }, walkable, sight);
-	}
+	path = search.AStarSearch(Point2{ seekerX, seekerY }, Point2{ destX, destY }, walkable, sight);
 
 	seeker->aiMovePath(path);
 }
 
 bool AIController::checkPossible(Point2 begin, Point2 key) {
-	std::vector<Point2> check = search.AStarSearch(begin, key, walkable, 0);
+	std::vector<Point2> check = search.AStarSearch(begin, key, walkable, 0); /** Data collection storing coords of calculated path */
 	if (!check.empty()) {
-		Point2 point = check[check.size() - 1];
+		Point2 point = check[check.size() - 1]; /** Last coord in provided path if not empty */
 		if (point.x == key.x && point.y == key.y) {
 			return true;
 		}
 	}
 	return false;
+}
+
+void AIController::DEBUG_ShowMap() {
+	for (int i = 0; i < mapX; i++) {
+		for (int j = 0; j < mapY; j++) {
+			std::cout << !walkable[i][j]->isWalkable() << ' ';
+		}
+		std::cout << std::endl;
+	}
 }
--- src/engine/ai_Sub/AIController.h
+++ src/engine/ai_Sub/AIController.h
@@ -11,24 +11,54 @@
 
 class AIController {
 	private:
-		int tileSize;
-		int mapX;
-		int mapY;
+		int tileSize;	/** Size of the tiles used in map generation */
+		int mapX;		/** Size of map along X coord */
+		int mapY;		/** Size of map along Y coord */
 
-		std::vector<std::vector<std::shared_ptr<Node> >> walkable;
+		std::vector<std::vector<std::shared_ptr<Node> >> walkable;	/** Data collection storing walkable and non walkable tiles */
 
-		std::vector<Point2> path;
-		AStar search;
-		//Point2 patrolCent;
+		std::vector<Point2> path;	/** Data collection storing coords of calculated path */
+
+		AStar search;	/** Reference to AStar algorithm */
+
 	public:
 		AIController();
 		~AIController();
 
-		void addMap(int, int, int, std::vector<std::shared_ptr<Rect>>);
+		/**
+		 *	Clears stored map data before adding new given map data to walkable, sets movable directions from each node.
+		 *	@param tile		size of tiles used in map
+		 *	@param xPixel	size of map along X coord
+		 *	@param yPixel	size of map along Y coord
+		 *	@param map		map data provided by game
+		 */
+		void addMap(int tile, int xPixel, int yPixel, std::vector<std::shared_ptr<Rect>> map);
 
-		void givePath(Entity*, Entity*);
+		/**
+		 *	If second Entity in range, path directly to it's location. Otherwise react to first Entity settings.
+		 *	E.G If first Entity is set to patrol, path to a random patrol point in it's sight radius
+		 *	@param seeker	The Entity that desires a path
+		 *	@param dest		The Primary target of first Entity.
+		 */
+		void givePath(Entity* seeker, Entity* dest);
+		/**
+		 * Override, path Entity straight to specific point
+		 * @param seeker	The Entity that desires a path
+		 * @param dest		The destination tile location
+		 */
 		void givePath(Entity*, Point2);
 
+		/**
+		 * Generate a path from one point to another and return if one exists or not
+		 * @param begin		The start location of the search
+		 * @param key		The key end point of the search
+		 * @return			Whether or not a complete path exists
+		 */
 		bool checkPossible(Point2 begin, Point2 key);
+
+		/** 
+		 *	Function intended for the debug of map data, prints map to console
+		 */
+		void DEBUG_ShowMap();
 };
 #endif
