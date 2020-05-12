7901727426d8cd64e8c0b936793a162bf36e4c46
Andrew Lee <oliver.lee@hotmail.co.uk>
2020-05-12 01:00:56 +0100

    ammended comments

--- src/test/TestGame.h
+++ src/test/TestGame.h
@@ -12,57 +12,73 @@ struct GameKey {
 
 class TestGame : public AbstractGame {
 	private:
-		TTF_Font * font;
-		TTF_Font * amaticFont;
-		TTF_Font * fontSmall;
+		TTF_Font * amaticFont; /**< Primary Font for TestGame */
+		TTF_Font * fontSmall; /**< Secondary Font for TestGame, buttons */
 
-		SDL_Rect * bg;
+		SDL_Rect * bg; /**< Display area for Background image*/
 
 		Entity * player;
-		std::vector<Entity *> npcCollection;
+		std::vector<Entity *> npcCollection; /**< Data collection storing pointers to currently spawned NPC Entities */
 		Entity * npc;
 
-		Rect btnPlay = Rect(0, 0, 0, 0);
-		Rect btnDiff = Rect(0, 0, 0, 0);
+		Rect btnPlay = Rect(0, 0, 0, 0); /**< Start menu play button */
+		Rect btnDiff = Rect(0, 0, 0, 0); /**< Start menu difficulty switch button */
 
-		SDL_Texture * entityTexture;
-		SDL_Texture * imgWall;
-		SDL_Texture * imgBacking;
-		SDL_Texture * imgCoin;
-		Mix_Chunk * coin;
-		Mix_Chunk * aiCollide;
+		SDL_Texture * entityTexture;	/**< Texture for use on Entity */
+		SDL_Texture * npcTexture;		/**< Texture for use on npcs */
+		SDL_Texture * imgWall;			/**< Texture for use on walls */
+		SDL_Texture * imgBacking;		/**< Texture for use as background */
+		SDL_Texture * imgCoin;			/**< Texture for use as pickup coin */
+		Mix_Chunk * coin;				/**< Sound file to play as pickup coin */
+		Mix_Chunk * aiCollide;			/**< Sound File to play on npc collision with player */
 		
-		int width;
-		int height;
-		int tileSize;
-		int npcCount = 3;
-		bool playHover = false;
-		bool diffHover = false;
-		bool btnBreak = true;
-		bool resetBreak = false;
+		int width;					/**< Map width in pixels */
+		int height;					/**< map height in pixels */
+		int tileSize;				/**< Size of the tiles used for the map */
+		int npcCount = 3;			/**< Number of npcs to spawn, default to 3 */
+		bool playHover = false;		/**< Flag for player mouse hovering over play button */
+		bool diffHover = false;		/**< Flag for player mouse hovering over difficulty button */
+		bool btnBreak = true;		/**< Flag to stop continuous activation of button */
+		bool resetBreak = false;	/**< Opposing Flag to stop free up use of the button again */
 
-		Vector2i velocity;
-		Vector2i npcVel;
+		Vector2i velocity;			
+		Vector2i npcVel;			/**< Value added onto movement of an npc  */
 
 		MazeGenerator * gen;
-		AIController * theAI;
 
-		std::vector<std::shared_ptr<Rect>> wall;
-		std::vector<std::shared_ptr<Line2i>> lines;
-		std::vector<std::shared_ptr<GameKey>> points;
+		std::vector<std::shared_ptr<Rect>> wall;		/**< Data collection storing the generated walls of the map */
+		std::vector<std::shared_ptr<GameKey>> points;	/**< Data collection storing the generated coins of the map */
 
 		/* GAMEPLAY */
-		int score, keys, lives, scoreInc;
-		bool gameWon;
-		enum gameState { MENU, PAUSE, PLAY, WIN, LOSE, LIFELOSS };
+		int score, keys, lives;
+		int scoreInc;				/**< Score multiplayer based on difficulty setting */
+
+		/**
+		 * Enum keeping a record of the game state for appropriate reactions default to MENU
+		 */
+		enum gameState { 
+			MENU,		/**< enum value MENU. */
+			PAUSE,		/**< enum value PAUSE. */
+			PLAY,		/**< enum value PLAY. */
+			WIN,		/**< enum value WIN. */
+			LOSE,		/**< enum value LOSE. */
+			LIFELOSS	/**< enum value LIFELOSS. */
+		};
 		gameState state = MENU;
 
 		void handleKeyEvents();
 		void update();
 		void render();
 		void renderUI();
+		/**
+		 * Deals with the interactivity and functionality of the start menu buttons
+		 */
 		void handleMenu();
+		/**
+		 * executes the map and coin generation code
+		 */
 		void generateLevel();
+
 	public:
 		TestGame();
 		~TestGame();
